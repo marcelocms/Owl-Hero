9a68a7b765158f292815c1bebc119e0ece1efd24
APiligrim <anastasiyauraleva@gmail.com>
2021-03-27 19:19:57 -0400

    Add: Basic folder setup

---
+++ owlhero/src/LandingPage.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./LandingPage.css";
+
+function LandingPage() {
+  return <div className="App">This is the landing page</div>;
+}
+
+export default LandingPage;
