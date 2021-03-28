ae545d695c3306c5320455424ae2767557634d66
APiligrim <anastasiyauraleva@gmail.com>
2021-03-28 03:31:09 -0400

    Added: a bit of responsiveness and added logo

--- owlhero/src/LandingPage.js
+++ owlhero/src/LandingPage.js
@@ -1,12 +1,14 @@
-import logo from "./logo.svg";
+import react from "react";
 import "./LandingPage.css";
 import HeaderBackground from "./components/img/backgrounds_Head.gif";
-import HeroCard from "./components/img/Hero_card.png";
-import OwlCard from "./components/img/Owl_card.png";
+import HeroCard from "./components/img/hero_card.png";
+import OwlCard from "./components/img/owl_card.png";
 import Menu from "./components/img/Menu_Footer.png";
 import Navbar from "../src/components/navigation/navigation";
 import TeacherTitle from "./components/img/teachers.png";
+import OurImpact from "./components/img/impact.png";
 import Galaxy from "./components/img/galaxy.gif";
+import footer from "./components/img/footer.png";
 
 function LandingPage() {
   return (
@@ -22,11 +24,11 @@ function LandingPage() {
           </div>
         </section>
 
-        {/* <img className="menu-footer fixed-menu" src={Menu} /> */}
-
         <img src={HeaderBackground} />
-        <img src={TeacherTitle} />
+        <img className="teachers-title" src={TeacherTitle} />
         <img src={Galaxy} />
+        <img className="our-impact-title" src={OurImpact} />
+        <img className="our-impact-title" src={footer} />
 
         <div className="footer">
           <button className="header-button">Install Now</button>
--- owlhero/src/components/navigation/navigation.js
+++ owlhero/src/components/navigation/navigation.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./navigation.css";
+import logo from "../img/logo.svg";
 
 function Navbar() {
   return (
-    <nav className="header sticki">
-      <div className="navbar"></div>
-    </nav>
+    <div>
+      <nav className="header sticki">
+        <div className="navbar"></div>
+      </nav>
+      <img className="logo sticki" src={logo} />
+    </div>
   );
 }
 
